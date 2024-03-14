7f71c94569624f467d3ad08199db954d8e09a28f
aidenwen <wenzel.aiden@outlook.com>
2024-03-14 17:41:42 -0400

    wrote dealer test

--- test/dealer_test.cpp
+++ test/dealer_test.cpp
@@ -1,4 +1,5 @@
 #include <gtest.h>
+#include <fstream>
 
 #include "card.hpp"
 #include "deck.hpp"
@@ -10,6 +11,21 @@ TEST(dealer, dealer_constructor) {
 
     ASSERT_EQ(dealer.get_card_sum(), 0);
     ASSERT_EQ(dealer.get_hand_size(), 0);
+}
+
+TEST(dealer, dealer_deal) {
+    std::ifstream suits_in("../black_jack/suits.in");
+    std::ifstream ranks_in("../black_jack/ranks.in");
+    Deck deck(suits_in, ranks_in);
+    Player player("Aiden", 0);
+    Dealer dealer;
+
+    Player* ptr_aiden = &player;
+    Deck* ptr_deck = &deck;
+    
+    dealer.deal(ptr_aiden, ptr_deck);
+
+    ASSERT_EQ(ptr_aiden->get_card_at(0), Card("two", "spades"));
 }
 
 int main(int argc, char **argv) {
