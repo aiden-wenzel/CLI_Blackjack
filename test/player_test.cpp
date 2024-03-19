febfdd7e092faa2d17ed84dabc38e88845f33738
aidenwen <wenzel.aiden@outlook.com>
2024-03-18 22:39:03 -0400

    wrote place_bet test

--- test/player_test.cpp
+++ test/player_test.cpp
@@ -13,6 +13,22 @@ TEST(player_test, player_constructor) {
     ASSERT_EQ(player.get_name(), "Aiden");
     ASSERT_EQ(player.get_money(), 45.04);
 }
+
+TEST(player_test, player_place_bet) {
+    Player aiden("Aiden", 20);
+    ASSERT_EQ(aiden.get_money(), 20);
+
+    std::stringstream expected_output;
+    expected_output << "Enter the amount of money you would like to bet:\n";
+
+    std::stringstream actual_output;
+
+    std::string input_string = "20";
+    std::istringstream input(input_string);
+    
+    ASSERT_EQ(aiden.place_bet(actual_output, input), 20);
+    ASSERT_EQ(actual_output.str(), expected_output.str());
+}
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
